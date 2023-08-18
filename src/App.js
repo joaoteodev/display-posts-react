b9a9801821a7508de86034b1f528bfa89a61eb48
João Pedro <joaodev84@gmail.com>
2023-08-18 18:42:20 -0300

    First version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://jsonplaceholder.typicode.com/";
+
+export class App extends Component {
+  state = {
+    posts: []
+  };
+
+  getPosts = async () => {
+    const response = await fetch(url + "posts");
+    const data = await response.json();
+    this.setState({ posts: data });
+  };
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
+  deletePost = post => {
+    const { posts } = this.state;
+    this.setState({
+      posts: posts.filter(p => p.id !== post.id)
+    });
+    console.log(post);
+  };
+
+  handleLikePost = postLiked => {
+    const { posts } = this.state;
+
+    this.setState({
+      posts: posts.map(post =>
+        post.id === postLiked.id ? { ...post, liked: !post.liked } : post
+      )
+    });
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {posts.map(post => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <span
+                onClick={() => this.handleLikePost(post)}
+                style={{ cursor: "pointer", userSelect: "none" }}
+              >
+                {post?.liked ? "❤" : "♡"}
+              </span>
+              <span
+                onClick={() => this.deletePost(post)}
+                style={{
+                  cursor: "pointer",
+                  userSelect: "none",
+                  marginLeft: 20
+                }}
+              >
+                🗑
+              </span>
+            </div>
+          ))}
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
